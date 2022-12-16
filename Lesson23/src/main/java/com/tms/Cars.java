e44596175a1343727ed3e52d0bd82d16d1e5561c
GlebGukov <Gleb_Gukov@inbox.ru>
2022-12-16 12:30:13 +0300

    Lesson 23. First commit

---
+++ Lesson23/src/main/java/com/tms/Cars.java
@@ -0,0 +1,22 @@
+package com.tms;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet ("/cars")
+public class Cars extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ServletOutputStream outputStream = resp.getOutputStream();
+        outputStream.println("Cars");
+
+    }
+
+
+
+}
