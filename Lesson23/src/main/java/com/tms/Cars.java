3623894e1cc419576283363afa16ad382c3199fc
GlebGukov <Gleb_Gukov@inbox.ru>
2022-12-17 12:04:29 +0300

    Lesson 23. Second commit

--- Lesson23/src/main/java/com/tms/Cars.java
+++ Lesson23/src/main/java/com/tms/Cars.java
@@ -1,22 +1,26 @@
 package com.tms;
 
 import javax.servlet.ServletException;
-import javax.servlet.ServletOutputStream;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Enumeration;
 
 @WebServlet ("/cars")
 public class Cars extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        ServletOutputStream outputStream = resp.getOutputStream();
-        outputStream.println("Cars");
+        String one = req.getParameter("one"); //вытаскиваем машину по ключу
+        System.out.println(one);
+
+        Enumeration<String> parameterNames = req.getParameterNames(); //вытаскиваем все машины
+        while (parameterNames.hasMoreElements()){
+            String a = parameterNames.nextElement();
+            System.out.println("Cars "+a+" "+ req.getParameter(a)) ;
+        }
+
 
     }
-
-
-
 }
