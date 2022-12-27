d3ac187655bfeb385ba9e3b5a9e7db331db214c9
GlebGukov <Gleb_Gukov@inbox.ru>
2022-12-27 12:31:36 +0300

    Lesson 26. First commit

---
+++ Lesson26/src/main/java/com/tms/CarsServNew.java
@@ -0,0 +1,10 @@
+package com.tms;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet("/cars")
+public class CarsServNew extends HttpServlet {
+
+
+}
